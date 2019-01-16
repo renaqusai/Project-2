ba3b0143001ac6b5710ef24adfe066eae8ba659e
Tej Poudel <newsbinodp@gmail.com>
2019-01-15 19:03:10 -0800

    Added dev

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+//Do not push it.
+
+module.exports = {
+    googleClientID: '371458567211-5kcnbgqa3mjh2uaml3gtqed293f1du6r.apps.googleusercontent.com',
+    googleClientSecret: 'MRNJ8zobJ1e_vKC--Mtu2m2P',
+    mongoURI: 'mongodb://tej:password1@ds039017.mlab.com:39017/emaily-dev',
+    kookieKey: 'dkfjeijfkjfjkdjfoeijfijofjsoijfjsffjfjjij'
+}
+
+
