efc53c2f98d48669abc8369a82d7e2f9296a912f
kallistahalim <kallistahalim@gmail.com>
2019-01-16 21:41:16 -0800

    try again

--- config/keys.js
+++ config/keys.js
@@ -3,4 +3,5 @@ if(process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
    module.exports = require('./dev');
-}
+};
+
--- server.js
+++ server.js
@@ -16,17 +16,33 @@ const app = express();
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
+        keys: [keys.cookieKey] 
     })
 );
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());                      
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/recipe_controller");
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
 
+app.use(routes);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
-    console.log("Server is running on localhost " + PORT);
+    console.log("Server is running on http://localhost:" + PORT);
 });
 
 
