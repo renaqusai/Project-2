96b40b2b035cd18355cc088421bee614516e4fff
kallistahalim <kallistahalim@gmail.com>
2019-01-19 19:35:42 -0800

    API is linked search recipe page

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
@@ -9,7 +10,7 @@ const axios = require('axios');
 var recipe = require("../models/recipemodel");
 
 
- router.get("/", function (req, res) {
+router.get("/", function (req, res) {
 res.render("index");
 });
 
@@ -49,38 +50,8 @@ res.render("index");
 const api_key = process.env.API_KEY;
 
 
-const queryURL = "https://api.edamam.com/search?q=&app_id=e3c21f1d&app_key=3e40f04f482e04daac9d6917ba78643f&from=0&to=3&calories=591-722";
-const displayRecipes = () => {
-axios.get(queryURL).then((response) => {
-	        // console.log(response.data.hits[0].recipe);
-const data = response.data.hits[0].recipe;
 
 
-            // output = space + header + space + space
-            // space + "Name           :" + data.label +
-            //     space + "source         :" + data.uri +
-            //     space + "Ingredients    :" + data.ingredients;
-
-         // <<<<<<< HEAD
-            // console.log(output);	
-
-         //             // console.log(output);
- 
-        
-        
-        console.log(response.data.hits[0].recipe.label);
-        console.log(response.data.hits[0].recipe.ingredientLines);
-        console.log(response.data.hits[0].recipe.totalTime);
-        console.log(response.data.hits[0].recipe.uri); //instructions
-        console.log(response.data.hits[0].recipe.image);
-        console.log(response.data.hits[0].recipe.healthLabels);
-        console.log(response.data.hits[0].recipe.dietLabels);
-        console.log(response.data.hits[0].recipe.calories);
-
-     });
-};
-displayRecipes();
-=======
 // Landing page
 router.get("/", (req, res) => {
     res.render("index");
